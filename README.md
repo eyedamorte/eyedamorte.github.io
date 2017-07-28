77ecb7a66d46dd232a361e161772c4f26263db42
root <root@rexdamorte>
2017-07-28 22:31:55 +0500

    add .

--- README.md
+++
@@ -1,1 +0,0 @@
-# eyedamorte.github.io
--- scripts/common.js
+++ scripts/common.js
@@ -1,5 +1,22 @@
 $(document).ready(function() {
     $('.slider_nav').slick({
-        arrows:true
+        adaptiveHeight: true,
+       arrows: true ,
+        responsive: [
+            {
+                breakpoint: 765,
+                settings: {
+                    arrows:false
+                }
+            }]
+    });
+
+    $(".menu-trigger").click(function () {
+        $("nav ul").slideToggle(500);
+    });
+    $(window).resize(function () {
+        if($(window).width()>765){
+            $("nav ul").removeAttr("style");
+        }
     });
 });
